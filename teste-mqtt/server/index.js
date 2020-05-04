c13d24b8e029dcdb3ddd9d55b458b0fde4733469
kaline <mesquitabfkaline@gmail.com>
2020-05-04 09:42:57 -0300

    :fire:rm all project

--- teste-mqtt/server/index.js
+++
@@ -1,7 +0,0 @@
-var aedes = require('aedes')()
-var server = require('net').createServer(aedes.handle)
-var port = 1883
-
-server.listen(port, function () {
-	console.log('server listening on port', port)
-})
