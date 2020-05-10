6feea0e8acd3aaa563da26e142645696d0488454
kaline <mesquitabfkaline@gmail.com>
2020-05-10 00:16:14 -0300

    :lipstick: + :rocket: add good interface to deploy

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
-const http = require('http');
+
+var express = require('express')
+var app = express()
+var path = require('path');
+
+app.use(express.static(path.join('public')));
+app.use("/css", express.static('css'));
+app.use("/html", express.static('html'));
 
 const hostname =  '127.0.0.1';
 const port = 3000;
 
-
-const server = http.createServer((req, res) => {
-
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Kaline');
-	
+app.get('/', function(req, res){
+	res.sendFile(path.join('public' +'index.html'));
 });
+app.get('events', function(req, res){
+	res.sendFile(path.join('public' + 'talks.html'));
+});
+app.get('projects', function(res, req){
+	res.sendFile(path.join('public' + 'projects.html'))
+})
 
-server.listen(port, hostname, () => {
-	console.log('Server running at http://${hostname}:${port}/');
+
+
+app.listen(port, hostname, () => {
+	console.log('Server running at http:///', hostname + ':' + port);
 });
